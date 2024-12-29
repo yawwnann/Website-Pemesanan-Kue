7cf02f42377abc59f3bfcf95e276ad84a646d086
Yuwananta Valencia Afshandy <c0478592@gmail.com>
2024-12-30 05:52:43 +0700

    Update midtrans_config.php

--- config/midtrans_config.php
+++ config/midtrans_config.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-\Midtrans\Config::$serverKey = 'SB-Mid-server-OmWvoWf7mz1mZELRQbfgg_Q2';  // Server Key Anda
-\Midtrans\Config::$clientKey = 'SB-Mid-client-PEVLiowZwZNrnJPX';  // Client Key Anda
+\Midtrans\Config::$serverKey = '';  // Server Key Anda
+\Midtrans\Config::$clientKey = '';  // Client Key Anda
 \Midtrans\Config::$isProduction = false; // Pastikan menggunakan sandbox mode
 \Midtrans\Config::$isSanitized = true;
 \Midtrans\Config::$is3ds = true;
